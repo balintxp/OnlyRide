41ebabc730378b039bdc8f18bb97b78585058b46
Máté Proszenyák <mate.proszenyak@stud.uni-corvinus.hu>
2026-04-21 20:10:05 +0200

    foglalási nézetek refaktorálása és SQL szinkronizálása a Figma dizájn alapján

--- OnlyRide.Dnn.ServiceBooking/Controllers/BookingController.cs
+++ OnlyRide.Dnn.ServiceBooking/Controllers/BookingController.cs
@@ -1,19 +1,8 @@
-﻿/*
-' Copyright (c) 2026 OnlyRide
-'  All rights reserved.
-' 
-' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-' DEALINGS IN THE SOFTWARE.
-' 
-*/
-
-using DotNetNuke.Entities.Users;
+﻿using DotNetNuke.Entities.Users;
 using DotNetNuke.Framework.JavaScriptLibraries;
 using DotNetNuke.Web.Mvc.Framework.ActionFilters;
 using DotNetNuke.Web.Mvc.Framework.Controllers;
+using DotNetNuke.Common;
 using OnlyRide.Dnn.ServiceBooking.Components;
 using OnlyRide.Dnn.ServiceBooking.Models;
 using System;
@@ -35,19 +24,17 @@ namespace OnlyRide.Dnn.ServiceBooking.Controllers
             return RedirectToDefaultRoute();
         }
 
-        public ActionResult Edit(int bookingId = -1)
+        public ActionResult Edit(int bookingId = -1, string date = "")
         {
             DotNetNuke.Framework.JavaScriptLibraries.JavaScript.RequestRegistration(CommonJs.DnnPlugins);
 
-            // Ez betölti a felhasználókat egy legördülő menühöz (opcionális, de meghagyjuk)
-            var userlist = UserController.GetUsers(PortalSettings.PortalId);
-            var users = from user in userlist.Cast<UserInfo>().ToList()
-                        select new SelectListItem { Text = user.DisplayName, Value = user.UserID.ToString() };
-
-            ViewBag.Users = users;
-
             var booking = (bookingId == -1 || bookingId == 0)
-                 ? new Booking { ModuleId = ModuleContext.ModuleId }
+                 ? new Booking
+                 {
+                     ModuleId = ModuleContext.ModuleId,
+                     CreatedOnDate = !string.IsNullOrEmpty(date) ? DateTime.Parse(date) : DateTime.Now,
+                     Status = "Függőben"
+                 }
                  : ServiceBookingManager.Instance.GetBooking(bookingId, ModuleContext.ModuleId);
 
             return View(booking);
@@ -57,40 +44,42 @@ namespace OnlyRide.Dnn.ServiceBooking.Controllers
         [DotNetNuke.Web.Mvc.Framework.ActionFilters.ValidateAntiForgeryToken]
         public ActionResult Edit(Booking booking)
         {
-            // Ha a BookingId 0 vagy -1, akkor ez egy új foglalás
-            if (booking.BookingId == -1 || booking.BookingId == 0)
+            try
             {
-                booking.UserId = User.UserID; // Az éppen bejelentkezett felhasználó ID-ja
-                booking.CreatedOnDate = DateTime.UtcNow; // UTC időt használunk a PDF utasítása alapján
-                booking.ModuleId = ModuleContext.ModuleId;
-
-                ServiceBookingManager.Instance.CreateBooking(booking);
-            }
-            else
-            {
-                // Meglévő foglalás frissítése
-                var existingBooking = ServiceBookingManager.Instance.GetBooking(booking.BookingId, ModuleContext.ModuleId);
-                if (existingBooking != null)
+                // Ha új foglalás, minden kötelező számot beállítunk 0-ra, ha üres lenne
+                if (booking.BookingId <= 0)
                 {
-                    existingBooking.SlotId = booking.SlotId;
-                    existingBooking.ServiceTypeId = booking.ServiceTypeId;
-                    existingBooking.CustomNote = booking.CustomNote;
-                    existingBooking.Status = booking.Status;
-                    // A szerelő által kitöltött mezők:
-                    existingBooking.ActualMinutes = booking.ActualMinutes;
-                    existingBooking.ActualPrice = booking.ActualPrice;
+                    booking.UserId = User.UserID;
+                    booking.ModuleId = ModuleContext.ModuleId;
+                    booking.SlotId = booking.SlotId > 0 ? booking.SlotId : 0;
+                    booking.ServiceTypeId = booking.ServiceTypeId > 0 ? booking.ServiceTypeId : 1;
+                    booking.EstimatedPrice = 0;
+                    booking.Status = "Függőben";
 
-                    ServiceBookingManager.Instance.UpdateBooking(existingBooking);
+                    ServiceBookingManager.Instance.CreateBooking(booking);
                 }
+                else
+                {
+                    var existing = ServiceBookingManager.Instance.GetBooking(booking.BookingId, ModuleContext.ModuleId);
+                    if (existing != null)
+                    {
+                        existing.ServiceTypeId = booking.ServiceTypeId;
+                        existing.CustomNote = booking.CustomNote;
+                        ServiceBookingManager.Instance.UpdateBooking(existing);
+                    }
+                }
+
+                // Ha a mentés sikerült, irány vissza a naptárhoz!
+                return RedirectToDefaultRoute();
             }
-
-            return RedirectToDefaultRoute();
+            catch (Exception ex)
+            {
+                // Ha üres oldalt kapsz, nézd meg jól a bal felső sarkot, ide írja a hibát!
+                return Content("MENTÉSI HIBA TÖRTÉNT: " + ex.Message);
+            }
         }
-
-        [ModuleAction(ControlKey = "Edit", TitleKey = "AddBooking")]
         public ActionResult Index()
         {
-            // Főoldal: kilistázzuk a foglalásokat
             var bookings = ServiceBookingManager.Instance.GetBookings(ModuleContext.ModuleId);
             return View(bookings);
         }
