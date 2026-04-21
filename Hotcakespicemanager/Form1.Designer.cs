3940bf45ae82dfad2b75aa2474ae6305cadafa80
olivmrs <oliver.vermes@stud.uni-corvinus.hu>
2026-04-21 18:28:07 +0200

    initial commit

---
+++ Hotcakespicemanager/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Hotcakespicemanager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            trackBar1 = new TrackBar();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            button3 = new Button();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            trackBar2 = new TrackBar();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Kategória";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(279, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Min ár";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(336, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Max ár";
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(393, 0);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(104, 45);
+            trackBar1.TabIndex = 3;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(0, 22);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(279, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(51, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(336, 22);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(51, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(619, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 8;
+            button1.Text = "Betöltés";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(713, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 9;
+            button2.Text = "Szűrés";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(0, 51);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(788, 332);
+            dataGridView1.TabIndex = 10;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(713, 415);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 11;
+            button3.Text = "Alkalmaz";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(169, 416);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 12;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(336, 415);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(121, 23);
+            comboBox3.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(28, 415);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 14;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(54, 397);
+            label4.Name = "label4";
+            label4.Size = new Size(44, 15);
+            label4.TabIndex = 15;
+            label4.Text = "Összeg";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(214, 398);
+            label5.Name = "label5";
+            label5.Size = new Size(25, 15);
+            label5.TabIndex = 16;
+            label5.Text = "-/+";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(366, 397);
+            label6.Name = "label6";
+            label6.Size = new Size(60, 15);
+            label6.TabIndex = 17;
+            label6.Text = "%/normál";
+            // 
+            // trackBar2
+            // 
+            trackBar2.Location = new Point(169, 0);
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(104, 45);
+            trackBar2.TabIndex = 18;
+            trackBar2.Scroll += trackBar2_Scroll;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(trackBar2);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(button3);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(trackBar1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TrackBar trackBar1;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private DataGridView dataGridView1;
+        private Button button3;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TrackBar trackBar2;
+    }
+}
---
+++ Hotcakespicemanager/Product.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Hotcakespicemanager
+{
+    public class Product
+    {
+        [JsonProperty("Bvin")]
+        public string Bvin { get; set; }
+
+        [JsonProperty("Sku")]
+        public string Sku { get; set; }
+
+        [JsonProperty("ProductName")]
+        public string ProductName { get; set; }
+
+        [JsonProperty("ListPrice")]
+        public decimal ListPrice { get; set; }
+
+        [JsonProperty("SitePrice")]
+        public decimal SitePrice { get; set; }
+
+        [JsonProperty("StoreId")]
+        public int StoreId { get; set; }
+
+        [JsonProperty("UrlSlug")]
+        public string UrlSlug { get; set; }
+
+        [JsonIgnore]
+        public decimal? NewPrice { get; set; }
+    }
+
+    public class ProductContent
+    {
+        [JsonProperty("Products")]
+        public List<Product> Products { get; set; }
+    }
+
+    public class ProductListResponse
+    {
+        [JsonProperty("Content")]
+        public ProductContent Content { get; set; }
+    }
+}
