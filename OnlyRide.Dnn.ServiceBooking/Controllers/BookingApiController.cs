6b769be7116307128566e0808ff2043c4609b7bf
Máté Proszenyák <mate.proszenyak@stud.uni-corvinus.hu>
2026-05-07 12:47:56 +0200

    foglalások lekérdezése API-n keresztül service type szerint

---
+++ OnlyRide.Dnn.ServiceBooking/Controllers/BookingApiController.cs
@@ -0,0 +1,45 @@
+﻿using DotNetNuke.Web.Api;
+using OnlyRide.Dnn.ServiceBooking.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace OnlyRide.Dnn.ServiceBooking.Controllers
+{
+    // Elérhető: /API/OnlyRide.Dnn.ServiceBooking/BookingApi/GetBookingsByServiceType?moduleId=X
+    public class BookingApiController : DnnApiController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IHttpActionResult GetBookingsByServiceType(int moduleId)
+        {
+            var bookings = ServiceBookingManager.Instance.GetBookings(moduleId);
+            var serviceTypes = ServiceBookingManager.Instance.GetServiceTypes(moduleId);
+
+            var result = serviceTypes.Select(st => new
+            {
+                ServiceTypeId = st.ServiceTypeId,
+                ServiceTypeName = st.Name,
+                BasePrice = st.BasePrice,
+                EstimatedMinutes = st.EstimatedMinutes,
+                BookingCount = bookings.Count(b => b.ServiceTypeId == st.ServiceTypeId && b.Status != "Lemondva"),
+                Bookings = bookings
+                    .Where(b => b.ServiceTypeId == st.ServiceTypeId && b.Status != "Lemondva")
+                    .Select(b => new
+                    {
+                        b.BookingId,
+                        b.Status,
+                        b.CreatedOnDate,
+                        b.ActualPrice,
+                        b.ActualMinutes,
+                        b.CustomNote
+                    })
+                    .OrderBy(b => b.CreatedOnDate)
+                    .ToList()
+            }).ToList();
+
+            return Json(result); // Ok() helyett Json()
+        }
+    }
+}
---
+++ OnlyRide.Dnn.ServiceBooking/RouteMapper.cs
@@ -0,0 +1,18 @@
+﻿using DotNetNuke.Web.Api;
+
+namespace OnlyRide.Dnn.ServiceBooking
+{
+    public class RouteMapper : IServiceRouteMapper
+    {
+        public void RegisterRoutes(DotNetNuke.Web.Api.IMapRoute mapRouteManager)
+        {
+            mapRouteManager.MapHttpRoute(
+                moduleFolderName: "OnlyRide.Dnn.ServiceBooking",
+                routeName: "default",
+                url: "{controller}/{action}",
+                defaults: new { },
+                namespaces: new[] { "OnlyRide.Dnn.ServiceBooking.Controllers" }
+            );
+        }
+    }
+}
